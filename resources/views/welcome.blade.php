11ee7ec77603bf406c30bccad1a15e6a782b93c8
Benben <benben4567@gmail.com>
2020-11-01 17:18:57 +0700

    fix view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <title>News Scraper</title>
 
@@ -81,38 +81,39 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    News Scraper
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-header text-center">
-                        <strong>Status</strong>
-                    </div>
-                    <div class="card-body">
-                        <dl class="row">
-                            <dt class="col-sm-5">Status</dt>
-                            <dd class="col-sm-7">: <span class="text-success">Running <i class="fas fa-check-circle"></i></span></dd>
-                            <dt class="col-sm-5">Last Checked</dt>
-                            <dd class="col-sm-7">: <i>{{ \Carbon\Carbon::createFromTimeStamp(strtotime($data->created_at))->diffForHumans() }}</i></dd>
-                            <dt class="col-sm-5">Last Status</dt>
-                            <dd class="col-sm-7">: <i> {{ $data->description }} </i></dd>
-                        </dl>
+        <div class="container-fluid">
+            <div class="flex-center position-ref full-height">
+                <div class="content">
+                    <div class="title mt-5 mb-4">
+                        News Scraper
                     </div>
                 </div>
             </div>
-        </div>
-
-        <footer class="footer">
-            <div class="container text-center">
-                <span class="text-muted">Made by accident | www.benbagus.com</span>
+            <div class="row justify-content-center">
+                <div class="col-sm-10 col-md-6 col-lg-4">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <strong>Status</strong>
+                        </div>
+                        <div class="card-body">
+                            <dl class="row">
+                                <dt class="col-sm-5">Status</dt>
+                                <dd class="col-sm-7">: <span class="text-success">Running <i class="fas fa-check-circle"></i></span></dd>
+                                <dt class="col-sm-5">Last Checked</dt>
+                                <dd class="col-sm-7">: <i>{{ \Carbon\Carbon::createFromTimeStamp(strtotime($data->created_at))->diffForHumans() }}</i></dd>
+                                <dt class="col-sm-5">Last Status</dt>
+                                <dd class="col-sm-7">: <i> {{ $data->description }} </i></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </footer>
+        </div>
+            <footer class="footer">
+                <div class="container text-center">
+                    <span class="text-muted">Made by accident | www.benbagus.com</span>
+                </div>
+            </footer>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
